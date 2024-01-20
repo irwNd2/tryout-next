5c2a638be3c9041a37ff91518c6d14b63b03393c
irwNd2 <irwandduadua@gmail.com>
2024-01-20 15:54:36 +0700

    token, session and adding custom session field

--- auth.ts
+++ auth.ts
@@ -1,7 +1,22 @@
-import NextAuth from "next-auth";
+import NextAuth, { Session, DefaultSession } from "next-auth";
 import authConfig from "@/auth.config";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "./lib/db";
+import { db } from "@/lib/db";
+import { JWT } from "next-auth/jwt";
+import { getUserById } from "@/data/user";
+import { type UserRole } from "@prisma/client";
+
+//separate this into a file like next-auth.d.ts
+export type ExtendedUser = DefaultSession["user"] & {
+  role: UserRole;
+};
+
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
+//separate this into a file like next-auth.d.ts
 
 export const {
   handlers: { GET, POST },
@@ -9,6 +24,24 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    async session({ session, token }: { token?: JWT; session: Session }) {
+      if (token?.sub && session.user) {
+        session.user.id = token?.sub;
+      }
+      if (token?.role && session.user) {
+        session.user.role = token.role as UserRole;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const user = await getUserById(String(token.sub));
+      if (!user) return token;
+      token.role = user.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: {
     strategy: "jwt",
