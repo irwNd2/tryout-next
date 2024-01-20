dda7eaa3211d0fabe6b4ec1109df1a9b980c941f
irwNd2 <irwandduadua@gmail.com>
2024-01-20 17:32:30 +0700

    login and register using google and github account

--- auth.config.ts
+++ auth.config.ts
@@ -4,8 +4,19 @@ import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import bcryptjs from "bcryptjs";
 
+import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
+
 export default {
   providers: [
+    Github({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     Credentials({
       async authorize(credentials) {
         const validateFields = await LoginSchema.safeParse(credentials);
--- components/auth/Social.tsx
+++ components/auth/Social.tsx
@@ -1,17 +1,35 @@
-"use strict";
+"use client";
 
+import { signIn } from "next-auth/react";
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebook } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { DEFAULT_LOGIN_REDIRECT_URL } from "@/routes";
 
 export const Social = () => {
+  const sosialLogin = (provider: "google" | "github") => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT_URL,
+    });
+  };
+
   return (
     <div className='flex items-center w-full gap-x-2'>
-      <Button variant='outline' className='w-full' size='lg' onClick={() => {}}>
+      <Button
+        variant='outline'
+        className='w-full'
+        size='lg'
+        onClick={() => sosialLogin("google")}
+      >
         <FcGoogle className='w-5 h-5' />
       </Button>
-      <Button variant='outline' className='w-full' size='lg' onClick={() => {}}>
-        <FaFacebook className='w-5 h-5' />
+      <Button
+        variant='outline'
+        className='w-full'
+        size='lg'
+        onClick={() => sosialLogin("github")}
+      >
+        <FaGithub className='w-5 h-5' />
       </Button>
     </div>
   );
